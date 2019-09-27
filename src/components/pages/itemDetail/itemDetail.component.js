6b74d1372bfe48e29a9177fb87c22a0d5f672f96
milkcan <milkcanbass@gmail.com>
2019-09-27 15:52:50 -0400

    update

--- src/components/pages/itemDetail/itemDetail.component.js
+++ src/components/pages/itemDetail/itemDetail.component.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './itemDetail.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectMyShopDataObj } from '../../../redux/myShop/myShop.selectors';
+import { selectMyShopDataObj, selectMyShopData } from '../../../redux/myShop/myShop.selectors';
 
 const ItemDetailPage = ({ match, myShopData }) => {
   console.log(match.params);
+  console.log(match.params.title);
+  console.log(match.params.id);
+  console.log(myShopData[match.params.title].items[match.params.id]);
 
   return <div>Item detail</div>;
 };
 
 const mapStateToProps = createStructuredSelector({
-  myShopData: selectMyShopDataObj,
+  myShopData: selectMyShopData,
 });
 
 export default connect(mapStateToProps)(ItemDetailPage);
