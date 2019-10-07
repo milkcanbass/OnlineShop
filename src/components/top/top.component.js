03741d23749e15ab4838703c768b50633e852af2
milkcan <milkcanbass@gmail.com>
2019-10-06 22:06:23 -0400

    fix issue

--- src/components/top/top.component.js
+++ src/components/top/top.component.js
@@ -3,12 +3,11 @@ import './top.styles.scss';
 
 const Top = () => (
   <div className="topContainer">
-    <div className="topImg">
-      <div className="topTitle">
-        <div className="text">SHINCAT</div>
-        <div className="text">&</div>
-        <div className="text">HANDOG</div>
-      </div>
+    {/* <div className="topImg" /> */}
+    <div className="topTitle">
+      <div className="text">SHINCAT</div>
+      <div className="text">&</div>
+      <div className="text">HANDOG</div>
     </div>
   </div>
 );
