a711c93e5eabb95a6ff68dba622e25f44749290a
milkcan <milkcanbass@gmail.com>
2019-10-09 12:54:51 -0400

    moving bar

--- src/components/signInSignUpTitle/signInSignUpTitle.compornent.js
+++ src/components/signInSignUpTitle/signInSignUpTitle.compornent.js
@@ -4,43 +4,45 @@ import { toggleSignInPage } from '../../redux/signInSinUp/signInSinUp.action';
 import './signInSignUpTitle.styles.scss';
 
 class SignInSignUpTitle extends Component {
-  componentDidUpdate(nextProps, nextState) {
-    if (this.props.signIn != nextProps.signIn) {
-      const active = document.querySelector('.active.title');
-      console.log(active);
+  // For the case initial render didn't draw line
+  componentDidMount() {
+    // const target = document.querySelector('.target');
+    // console.log(target);
+    // const active = document.querySelector('.active.title');
+    // const left = active.getBoundingClientRect();
+    // const left = active.getBoundingClientRect().left + window.pageXOffset;
+    // const top = active.getBoundingClientRect().top + window.pageYOffset;
+    // const { width, height } = rect;
+    // target.style.width = `${width}px`;
+    // target.style.height = `${height}px`;
+    // target.style.left = `${left}px`;
+    // target.style.top = `${top}px`;
+    // target.style.borderColor = 'red';
+    // target.style.transform = 'none';
+    // }
+  }
 
-      const rect = active.getBoundingClientRect();
-      console.log(rect);
-    }
+  componentDidUpdate() {
+    // if (this.props.signIn != nextProps.signIn) {
+    const target = document.querySelector('.target');
+    const active = document.querySelector('.active.title');
+    console.log(active);
+    const rect = active.getBoundingClientRect();
+    const left = active.getBoundingClientRect().left + window.pageXOffset;
+    const top = active.getBoundingClientRect().top + window.pageYOffset + 3;
+    const { width, height } = rect;
+    target.style.width = `${width}px`;
+    target.style.height = `${height}px`;
+    target.style.left = `${left}px`;
+    target.style.top = `${top}px`;
+
+    console.log(left, rect.left);
+
+    // }
   }
 
   render() {
     const { toggleSignInPage, signIn } = this.props;
-
-    function test() {
-      // const { height } = target.getBoundingClientRect();
-      // const left = target.getBoundingClientRect().left + window.pageXOffset;
-      // const top = target.getBoundingClientRect().top + window.pageYOffset;
-      // const color = colors[Math.floor(Math.random() * colors.length)];
-      // target.style.width = `${width}px`;
-      // target.style.height = `${height}px`;
-      // target.style.left = `${left}px`;
-      // target.style.top = `${top}px`;
-      // target.style.borderColor = color;
-      // target.style.transform = 'none';
-      // function resizeFunc() {
-      //   const active = document.getElementsByClassName('active');
-      //   if (active) {
-      //     const left = active.getBoundingClientRect().left + window.pageXOffset;
-      //     const top = active.getBoundingClientRect().top + window.pageYOffset;
-      //     target.style.left = `${left}px`;
-      //     target.style.top = `${top}px`;
-      //   }
-      // }
-      // window.addEventListener('resize', resizeFunc);
-    }
-
-    test();
 
     return (
       <div className="wrapper">
@@ -51,16 +53,16 @@ class SignInSignUpTitle extends Component {
               onClick={!signIn ? () => toggleSignInPage() : null}
             >
               <a>SIGN IN</a>
+              <span className="target" />
             </li>
             <li
               className={`${!signIn ? 'active' : ''} title`}
               onClick={signIn ? () => toggleSignInPage() : null}
             >
-              <a> SIGN UP!!!!</a>
+              <a> SIGN UP</a>
             </li>
           </ul>
         </nav>
-        <span className="target" />
       </div>
     );
   }
