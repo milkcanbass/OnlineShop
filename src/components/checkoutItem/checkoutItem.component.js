f18d118c186070ca57921b3bd9a525eaeb76ab34
milkcan <milkcanbass@gmail.com>
2019-10-14 12:41:13 -0400

    update add function

--- src/components/checkoutItem/checkoutItem.component.js
+++ src/components/checkoutItem/checkoutItem.component.js
@@ -2,51 +2,60 @@ import React from 'react';
 import './checkoutItem.styles.scss';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.action';
+import { createStructuredSelector } from 'reselect';
+import { clearItemFromCart, removeItem } from '../../redux/cart/cart.action';
+import { addItemToCart } from '../../firebase/firebase.utils';
+import { selectCartId } from '../../redux/cart/cart.selectors';
 
-const CheckoutItem = ({
-  cartItem: {
-    name, imageUrl, price, quantity,
-  },
-  clearItemFromCart,
-  addItem,
-  removeItem,
-}) => (
-  <div className="checkoutItemContainer">
-    <div className="imageContainer">
-      <img src={imageUrl} alt="item" />
+const CheckoutItem = (props) => {
+  // console.log(props.cartItem);
+  const {
+    cartItem: {
+      name, imageUrl, price, quantity,
+    },
+    cartId,
+    clearItemFromCart,
+    removeItem,
+  } = props;
+  return (
+    <div className="checkoutItemContainer">
+      <div className="imageContainer">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div role="button" className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div role="button" className="arrow" onClick={() => addItemToCart(cartId, props.cartItem)}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <span role="button" className="removeButton" onClick={() => clearItemFromCart(cartItem)}>
+        &#10005;
+      </span>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">
-      <div role="button" className="arrow" onClick={() => removeItem(cartItem)}>
-        &#10094;
-      </div>
-      <span className="value">{quantity}</span>
-      <div role="button" className="arrow" onClick={() => addItem(cartItem)}>
-        &#10095;
-      </div>
-    </span>
-    <span className="price">{price}</span>
-    <span role="button" className="removeButton" onClick={() => clearItemFromCart(cartItem)}>
-      &#10005;
-    </span>
-  </div>
-);
+  );
+};
 
 CheckoutItem.protoTypes = {
-  cartItem: PropTypes.oneOfType(PropTypes.oneOfType([PropTypes.string, PropTypes.number])).isRequired,
+  cartItem: PropTypes.oneOfType(PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
+    .isRequired,
   clearItemFromCart: PropTypes.func.isRequired,
-  addItem: PropTypes.func.isRequired,
   removeItem: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
   clearItemFromCart: (item) => dispatch(clearItemFromCart(item)),
-  addItem: (item) => dispatch(addItem(item)),
   removeItem: (item) => dispatch(removeItem(item)),
+});
+const mapStateToProps = createStructuredSelector({
+  cartId: selectCartId,
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(CheckoutItem);
